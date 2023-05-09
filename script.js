2210d5dcfddeab04b91effd9245624955b4b71db
Kartikey Singh <84768512+KartikeySingh99@users.noreply.github.com>
2023-05-09 14:49:19 +0530

    changed click event

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let button = document.getElementById('speak');
 let keycode = 13;
-document.addEventListener('keyup', (event) => {
-    if (event.keyCode === 13) {
-        console.log("it works");
-        let msg = document.getElementById('myText').value;
-        let speech = new SpeechSynthesisUtterance();
-        speech.lang = "en-US";
-        speech.text = msg;
-        speech.rate = .7;
-        speech.pitch = 1;
-        if (msg == "") {
-            speech.text = "Please Type Some Text";
-            window.speechSynthesis.speak(speech);
-        }
-        else {
-            window.speechSynthesis.speak(speech);
-        }
+button.addEventListener('click', (event) => {
+    // if (event.keyCode === 13) {
+    console.log("it works");
+    let msg = document.getElementById('myText').value;
+    let speech = new SpeechSynthesisUtterance();
+    speech.lang = "en-US";
+    speech.text = msg;
+    speech.rate = .9;
+    speech.pitch = 1;
+    
+    if (msg == "") {
+        speech.text = "Please Type Some Text";
+        window.speechSynthesis.speak(speech);
     }
+    else {
+        window.speechSynthesis.speak(speech);
+    }
+    // }
 })
 
